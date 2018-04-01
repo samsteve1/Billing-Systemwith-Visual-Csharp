41f429d8a4e0f50859dc4e4ca4ee688ebc421ffb
Steve Idowu <idowusteve@ymail.com>
2018-04-01 12:47:32 +0100

    Added Customer BLL

---
+++ BirthmarkStore/BLL/DeaCust.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirthmarkStore.BLL
+{
+    class DeaCust
+    {
+        public int Id { get; set; }
+        public string  Type { get; set; }
+        public string Name { get; set; }
+        public string  Email { get; set; }
+        public string Contact { get; set; }
+        public string Address { get; set; }
+        public DateTime Added_date { get; set; }
+        public int Added_by { get; set; }
+    }
+}
